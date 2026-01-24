ca040f23a487a7ffa9beb6bb5a4816b363d6b975
JuanLasala <jlasala@correo.um.edu.uy>
2026-01-24 15:46:11 -0300

    agrego CORS a main

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.routers import router
 from app.db import db_client
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv("config/.env")
 
@@ -17,5 +18,13 @@ async def lifespan(app: FastAPI):
         await db_client.pool.wait_closed()
 
 app = FastAPI(title="wildfires-api", lifespan=lifespan)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
